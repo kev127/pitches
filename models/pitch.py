e789f2949bbe0c971d7de3df15f0fc3b0a8739d7
kev127 <kelvinkeya125@gmail.com>
2020-10-25 12:57:29 +0300

    set up configurations to init.py and add models.py

---
+++ models/pitch.py
@@ -0,0 +1,13 @@
+class Pitch:
+    '''
+    Pitch class to define Pitch Objects
+    '''
+
+    def __init__(self,id,title,content,category,user,likes,dislikes):
+        self.id =id
+        self.title = title
+        self.content = content
+        self.category = category 
+        self.user = User
+        self.likes = likes 
+        self.dislikes = dislikes

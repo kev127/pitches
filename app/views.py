1142cd74e3877311c2c2fefd15098e2269fecb4c
kev127 <kelvinkeya125@gmail.com>
2020-10-25 12:06:30 +0300

    add variable block to index.html

--- app/views.py
+++ app/views.py
@@ -8,4 +8,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    message = 'Pitches'
+    return render_template('index.html',message = message)

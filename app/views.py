679ef6d3df44f755960ed99b5cd61fad08610a5b
kev127 <kelvinkeya125@gmail.com>
2020-10-27 08:51:29 +0300

    add 404.html and error page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import app
 from .request import get_pitches
+from .forms import PitchForm
+
 
 # Views
 @app.route('/')
@@ -19,3 +21,18 @@ def index():
 
 
     return render_template('index.html',title = title, interview = interview_pitches, product = product_pitches, promotion = promotion_pitches)
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)

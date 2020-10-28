f01b7d70299b621d6cefc8f7ec79c80b9bdbf612
kev127 <kelvinkeya125@gmail.com>
2020-10-28 08:15:23 +0300

    add update profile and comment form

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,3 +8,11 @@ class PitchForm(FlaskForm):
     text = TextAreaField('Text',validators=[Required()])
     category = SelectField('Type',choices=[('interview','Interview pitch'),('product','Product pitch'),('promotion','Promotion pitch')],validators=[Required()])
     submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    text = TextAreaField('Leave a comment:',validators=[Required()])
+    submit = SubmitField('Submit')
